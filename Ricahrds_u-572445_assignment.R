327c0e05ba1d37631b807c4118812e00116a40af
Mikhail Shaw <mikhailshaw@Mikhails-MacBook-Pro.local>
2019-11-19 09:34:35 -0500

    Updates Updated gitignore Updated answers Updated assignment format to for Assignment_1_DemoScript.R

--- Ricahrds_u-572445_assignment.R
+++ Ricahrds_u-572445_assignment.R
@@ -1,11 +1,14 @@
-# Byronie Richards u-572445
+# Byronie Richards 
+# u-572445
 
+## Load packages ---------------------------------------------------------------
 library(dplyr)
 library(ggplot2)
 
+## Load data -------------------------------------------------------------------
 videogames <- read.delim("./input/videogames.txt", stringsAsFactors = FALSE)
 
-################ Question #1 ################
+## Exercise 1 -----------------------------------------------------------------
 colNames <- colnames(videogames)
 
 onlyGlobalSalesVideogames <- videogames
@@ -17,7 +20,7 @@ answer1 <- onlyGlobalSalesVideogames[onlyGlobalSalesVideogames$Global_Sales >= 1
 # print(answer1[answer1$Global_Sales < 1, ])
 
 
-################ Question #2 ################
+## Exercise 2 -----------------------------------------------------------------
 
 videogamesWithAVGScore <- videogames
 videogamesWithAVGScore$Average_Score <- (videogamesWithAVGScore$Critic_Score + videogamesWithAVGScore$User_Score) / 2
@@ -26,7 +29,7 @@ videogamesWithAVGScore$Average_Score <- (videogamesWithAVGScore$Critic_Score + v
 answer2 <- videogamesWithAVGScore
 # str(answer2)
 
-################ Question #3 ################
+## Exercise 3 -----------------------------------------------------------------
 
 PlaformsAndGlobalSales <- videogames[ , names(videogames) == 'Name' | names(videogames) == 'Global_Sales'] 
 
@@ -37,7 +40,7 @@ PlaformsAndGlobalSales$Num_Platforms <- transform(PlaformsAndGlobalSales, Num_Pl
 answer3 <- PlaformsAndGlobalSales
 # str(answer3)
 
-################ Question #4 ################
+## Exercise 4 -----------------------------------------------------------------
 
 NoKAVideogames <- videogames
 NoKAVideogames$Rating[NoKAVideogames$Rating == 'K-A'] <- 'E10+'
@@ -45,45 +48,42 @@ NoKAVideogames$Rating[NoKAVideogames$Rating == 'K-A'] <- 'E10+'
 answer4 <- NoKAVideogames
 # print(answer4[answer4$Rating == 'K-A', ])
 
-################ Question #5 ################
+## Exercise 5 -----------------------------------------------------------------
 
-WiiUScores <- videogames[ videogames$Platform == 'WiiU' , names(videogames) == 'Platform' | names(videogames) == 'Critic_Score' | names(videogames) == 'User_Score']
+WiiUScores <- videogames[ videogames$Platform == 'WiiU' , names(videogames) == 'Platform' | names(videogames) == 'Critic_Score' | names(videogames) == 'User_Score' | names(videogames) == 'Critic_Count' | names(videogames) == 'User_Count']
 
 # str(WiiUScores)
 
-answer5 <- ggplot() + geom_point(data = videogames, mapping = aes(y = Critic_Score, x = User_Score, color = Critic_Score, size = User_Score)) + labs(title="WiiU Critic Scores by User Scores", x="User's Scores", y="Critic's Scores")
-# answer5
+answer5 <- ggplot() + 
+    geom_point(data = videogames, mapping = aes(y = Critic_Score, x = User_Score, color = Critic_Count, size = User_Count)) + 
+    labs(title="WiiU Critic Scores by User Scores", x="Users' Scores", y="Critics' Scores", color="Critic Count", size = "User Count") + 
+    scale_color_continuous(low = "white", high = "black")
+answer5
 
-################ Question #6 ################
+## Exercise 6 -----------------------------------------------------------------
 
-videogameRatings <- videogames[videogames$Rating != 'AO', names(videogames) == 'Name' | names(videogames) == 'Rating']
-videogameRatings <- videogameRatings[videogameRatings$Rating != 'EC', ]
-videogameRatings <- videogameRatings[videogameRatings$Rating != 'K-A', ]
-videogameRatings <- videogameRatings[videogameRatings$Rating != 'RP', ]
-videogameRatings <- na.omit(videogameRatings)
-
-videogameRatings <- arrange(videogameRatings, Rating)
-videogameRatings <- transform(videogameRatings, NumOfGames = ave(seq(nrow(videogameRatings)), Rating, FUN=length))
+videogameRatings <- videogames[, names(videogames) == 'Name' | names(videogames) == 'Rating']
 
 # str(videogameRatings)
 
-videogameRatings <- videogameRatings %>% count(Rating, NumOfGames)
 
-
-answer6 = ggplot(data = videogameRatings, mapping = aes(y = NumOfGames, x = Rating)) + geom_bar(stat="identity") + labs( x = 'Ratings', y = 'Number of Games', title = 'Number of Games per Rating')
+answer6 = ggplot(data = videogameRatings, mapping = aes( x = Rating)) + 
+            geom_bar() + 
+            scale_x_discrete(limits = c("E", "E10+", "T", "M"), labels = c("Everyone", "Everyone 10+", "Teen", "Mature")) + 
+            labs( x = 'Ratings', y = 'Number of Games', title = 'Number of Games per Rating')
 # answer6
 
-################ Question #7 ################
+## Exercise 7 -----------------------------------------------------------------
 
 UbiAct <- videogames[videogames$Publisher == 'Ubisoft' | videogames$Publisher == 'Activision', names(videogames) == 'Genre' | names(videogames) == 'Publisher']
 UbiAct <- na.omit(UbiAct)
 
-UbiAct <- UbiAct %>% count(Genre, Publisher, name="NumOfPublishes")
+# UbiAct <- UbiAct %>% count(Genre, Publisher, name="NumOfPublishes")
 
-answer7 <- ggplot(data = UbiAct, mapping = aes(y = NumOfPublishes, x = Genre, fill = Publisher)) + geom_col() + labs( x = 'Genre', y = 'Number of Publishes', title = "Number of Publishes per Genre for 'Ubisoft' and 'Activision'")
+answer7 <- ggplot(data = UbiAct, mapping = aes( x = Publisher, fill = Genre)) + geom_bar(position = "dodge") + labs( y = 'Genre', x = 'Number of Publishes', title = "Number of Publishes per Genre for 'Ubisoft' and 'Activision'")
 # answer7
 
-################ Question #8 ################
+## Exercise 8 -----------------------------------------------------------------
 
 GenreCritics <- videogames[ , names(videogames) == 'Genre' | names(videogames) == 'Critic_Score']
 
@@ -92,19 +92,23 @@ GenreCritics <- videogames[ , names(videogames) == 'Genre' | names(videogames) =
 answer8 <- ggplot(data = GenreCritics, mapping = aes( x = Genre, y = Critic_Score)) + geom_boxplot(outlier.colour="skyblue") + labs(x = 'Genre', y = 'Critic Scores', title = 'Distribution of Critic Scores by Genre')
 # answer8
 
-################ Question #9 ################
+## Exercise 9 -----------------------------------------------------------------
 
-legoGames <- videogames[grepl('LEGO', videogames$Name, fixed=TRUE) , names(videogames) == 'Name' | names(videogames) == 'User_Score' | names(videogames) == 'Global_Sales']
-legoGames <- legoGames %>% group_by(Name) %>% summarize(Global_Sales = sum(na.omit(Global_Sales)), User_Score = sum(na.omit(User_Score))) #legoGames[!duplicated(legoGames$Name), ]
-legoGames$User_Score_Mean <- mean(na.omit(legoGames[['User_Score']]))
-legoGames$Total_Global_Sales <- sum(na.omit(legoGames[['Global_Sales']]))
+legoGames <- videogames[grepl('LEGO', videogames$Name, fixed=TRUE) , names(videogames) == 'Name' | names(videogames) == 'User_Score' | names(videogames) == 'Global_Sales'] %>% 
+    group_by(Name) %>% summarize(Global_Sales = sum(na.omit(Global_Sales)), User_Score = sum(na.omit(User_Score))) #legoGames[!duplicated(legoGames$Name), ]
+User_Score_Mean <- mean(na.omit(legoGames[['User_Score']]))
+Total_Global_Sales <- sum(na.omit(legoGames[['Global_Sales']]))
 
 # str(legoGames)
 
-answer9 <- legoGames
+answer9 <- list(
+        user_score_mean = User_Score_Mean, 
+        total_global_sales = Total_Global_Sales,
+        all_lego_games = legoGames[ , names(legoGames) == 'Name']
+    )
 # str(answer9)
 
-################ Question #10 ################
+## Exercise 10 -----------------------------------------------------------------
 
 numGamesPerYear <- videogames[ , names(videogames) == 'Year_of_Release' | names(videogames) == 'Genre']
 
@@ -118,7 +122,7 @@ geom_line() + geom_point()
 
 # answer10
 
-################ Question #11 ################
+## Exercise 11 -----------------------------------------------------------------
 
 gameGenre <- videogames[ , names(videogames) == 'Name' | names(videogames) == 'Genre']
 
@@ -130,7 +134,7 @@ gameGenre <- gameGenre %>% count(Genre, NumOfGames)
 
 answer11 <- ggplot(data = gameGenre, mapping = aes(y = as(NumOfGames, 'integer'), x = Genre, fill=Genre)) + 
 geom_col() + labs( x = "", y = "Number of Games", title="Videogame Hits Per Genre") + 
-theme(legend.position="none", axis.text.y=element_blank()) + 
+theme(legend.position="none", axis.text.y=element_blank(), axis.ticks.y = element_blank()) + 
 geom_text(aes(label=NumOfGames), position=position_dodge(width=0.9), vjust=-0.5)
 
 # answer11
